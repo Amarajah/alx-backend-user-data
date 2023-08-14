786138f3e1762ea8d7728ae8fab209ac5c411b47
Amsrajah <naomi.ibeh69@gmail.com>
2023-08-14 21:45:23 +0000

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,34 @@
 """0. Regex-ing"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """function called filter_datum that returns the log message obfuscated"""
-    regex_pattern = f'({re.escape(separator.join(fields))})'
-    return re.sub(regex_pattern, redaction, message)
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """method to filter values in incoming log records using filter_datum.
+        Values for fields in fields should be filtered."""
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION, message,
+                                self.SEPARATOR)
+        return redacted
