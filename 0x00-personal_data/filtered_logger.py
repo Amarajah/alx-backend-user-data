8540cfb978401ebdc40291ee92355dc6a94160d8
Amsrajah <naomi.ibeh69@gmail.com>
2023-08-21 00:19:35 +0000

    logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-"""0. Regex-ing"""
+"""
+Definition of filter_datum function that returns an obfuscated log message
+"""
+from typing import List
 import re
+import logging
 import os
 import mysql.connector
-from typing import List
-import logging
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """function called filter_datum that returns the log message obfuscated"""
+    """
+    Return an obfuscated log message
+    Args:
+        fields (list): list of strings indicating fields to obfuscate
+        redaction (str): what the field will be obfuscated to
+        message (str): the log line to obfuscate
+        separator (str): the character separating the fields
+    """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
                          field+'='+redaction+separator, message)
@@ -29,50 +41,48 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """method to filter values in incoming log records using filter_datum.
-        Values for fields in fields should be filtered."""
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
         message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION, message,
-                                self.SEPARATOR)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
         return redacted
 
 
 def get_logger() -> logging.Logger:
-    """function that takes no arguments and returns a logging.Logger object."""
+    """
+    Return a logging.Logger object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
     handler = logging.StreamHandler()
+
     formatter = RedactingFormatter(PII_FIELDS)
+
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
-
     return logger
 
 
-PII_FIELDS = ("name", "email", "phone_number", "ssn", "password")
-
-
-def get_db():
-    """Connect to secure database"""
-    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    try:
-        db_connection = mysql.connector.connect(
-            host=db_host,
-            user=db_username,
-            password=db_password,
-            database=db_name
-        )
-        return db_connection
-    except mysql.connector.Error as err:
-        print(f"Error connecting to the database: {err}")
-        return None
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
+    return conn
 
 
 def main():
